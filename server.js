a127433089f2de822223008a38cc9582f702009e
Arthur Del Esposte <arthurmde@gmail.com>
2016-01-31 16:28:00 -0200

    Basic extension structure

---
+++ server.js
@@ -0,0 +1,7 @@
+//Verify the use of programmatic injection of the code to connect the server
+$ = jQuery;
+
+$(document).ready(function() {
+  console.log("Working");
+  alert('Basic Structure is working!');
+});
